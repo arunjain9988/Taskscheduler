22ff28b79914e84982eeb521d4846be4d32587f5
arun <arunjain9988@gmail.com>
2019-06-26 05:56:57 +0530

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/TaskScheduler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TaskScheduler.dir/main.cpp.o"
+  "TaskScheduler.pdb"
+  "TaskScheduler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TaskScheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TaskScheduler.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TaskScheduler.
+# This may be replaced when dependencies are built.
